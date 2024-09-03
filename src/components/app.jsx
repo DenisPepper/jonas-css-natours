de93146fdb36e287226a979958c7e6dbb21545aa
DenisPepper <thech@yandex.ru>
2024-09-03 16:41:34 +0300

    Перенесет проект на реакт

---
+++ src/components/app.jsx
@@ -0,0 +1,6 @@
+import './App.css';
+import { Header } from './header/header';
+
+export function App() {
+  return <Header />;
+}
---
+++ src/components/header/header.jsx
@@ -0,0 +1,21 @@
+import './header.css';
+import logo from '/src/assets/logo-white.png';
+
+export function Header() {
+  return (
+    <header className='header'>
+      <div className='header__logo-box'>
+        <img className='header__logo' src={logo} alt='Logo' />
+      </div>
+      <div className='header__text-box'>
+        <h1 className='heading-primary'>
+          <span className='heading-primary--main'>Outdoors</span>
+          <span className='heading-primary--sub'>is were life happens</span>
+        </h1>
+        <a className='btn btn--white btn--animated' href='#'>
+          Discover our tours
+        </a>
+      </div>
+    </header>
+  );
+}
