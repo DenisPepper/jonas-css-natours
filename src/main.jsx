53ad005137563a1ac8fc38cc84a939704c1c13b0
DenisPepper <thech@yandex.ru>
2024-09-13 19:46:09 +0300

    добавит схему разбивки ширины экрана для @media

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,18 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>
 );
+
+// responsive desing
+// https://youtu.be/-iqFeek9E7E
+
+/* desctop first
+
+  0 - 600 px   phone        @media(max-width: 600px)   phone
+600 - 900 px   tablet      
+900 - 1200 px  landscape
+
+normal syles
+
+1800 +         big screens
+
+*/
