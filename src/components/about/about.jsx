39f5debeb27d34601f8063e8429bfb35b2127d5e
DenisPepper <thech@yandex.ru>
2024-09-18 23:52:49 +0300

    Заменит обычную графику на адаптивную в секции About

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,11 +2,13 @@ import { HeadingSecondary as H2 } from '../heading-secondary/heading-secondary';
 import { HeadingTertiary as H3 } from '../heading-tertiary/heading-tertiary';
 import { Paragraph as P } from '../paragraph/paragraph';
 import { TextButton as A } from '../text-button/text-button';
-
 import './about.scss';
-import picPath1 from '/src/assets/nat-1-large.jpg';
-import picPath2 from '/src/assets/nat-2-large.jpg';
-import picPath3 from '/src/assets/nat-3-large.jpg';
+import picPath1 from '/src/assets/nat-1.jpg';
+import picPath2 from '/src/assets/nat-2.jpg';
+import picPath3 from '/src/assets/nat-3.jpg';
+import picPath1Large from '/src/assets/nat-1-large.jpg';
+import picPath2Large from '/src/assets/nat-2-large.jpg';
+import picPath3Large from '/src/assets/nat-3-large.jpg';
 
 const data = {
   header2: 'Exciting tours for adventurous people',
@@ -31,9 +33,28 @@ export function About() {
         </div>
         <div>
           <div className='composition'>
-            <img className='composition__photo composition__photo--p1' src={picPath1} alt='Photo 1' />
-            <img className='composition__photo composition__photo--p2' src={picPath2} alt='Photo 2' />
-            <img className='composition__photo composition__photo--p3' src={picPath3} alt='Photo 3' />
+            <img
+              className='composition__photo composition__photo--p1'
+              srcSet={`${picPath1} 300w, ${picPath1Large} 1000w`}
+              src={picPath1Large} // для старых браузеров
+              sizes='(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px'
+              alt='Photo 1'
+            />
+            <img
+              className='composition__photo composition__photo--p2'
+              srcSet={`${picPath2} 300w, ${picPath2Large} 1000w`}
+              src={picPath2Large} // для старых браузеров
+              sizes='(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px'
+              alt='Photo 2'
+            />
+            <img
+              className='composition__photo composition__photo--p3'
+              srcSet={`${picPath3} 300w, ${picPath3Large} 1000w`}
+              src={picPath3Large} // для старых браузеров
+              sizes='(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px'
+              alt='Photo 3'
+            />
+            
           </div>
         </div>
       </div>
